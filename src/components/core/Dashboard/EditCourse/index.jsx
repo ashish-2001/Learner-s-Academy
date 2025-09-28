ff3b5dbfd2eb26ce96c38eba7269bbc5b7685f48
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-09-28 16:37:53 +0530

    Added some of the components

---
+++ src/components/core/Dashboard/EditCourse/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import {
+    fetchCourseDetails,
+    getFullDetailsOfCourse
+} from "../../../../services/operations/courseDetailsAPI";
+import { setCourse, setEditCourse } from "../../../../slices/courseSlice";
+import { RenderSteps } from "../AddCourse/RenderSteps";
+
+function EditCourse(){
+    const dispatch = useDispatch();
+    const { courseId } = useParams();
+    const { course } = useSelector((state) => state.course);
+    const [loading, setLoading] = useState(false);
+    const { token } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        ;(async() => {
+            setLoading(true)
+            const result = await getFullDetailsOfCourse(courseId, token)
+            if(result?.courseDetails){
+                dispatch(setEditCourse(true))
+                dispatch(setCourse(result?.courseDetails))
+            }
+            setLoading(false)
+        })()
+    }, []);
+
+    if(loading){
+        return(
+            <div className="grid flex-1 place-items-center">
+                <div className="spinner"></div>
+            </div>
+        )
+    }
+
+    return(
+        <div className="mb-
+        9"></div>
+    )
+}
---
+++ src/components/core/Dashboard/MyCourses.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { VscAdd } from "react-icons/vsc";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { fetchInstructorCourses } from "../../../services/operations/courseDetailsAPI";
+import { IconBtn } from "../../Common/IconBtn";
+import { CoursesTable } from "./InstructorCourses/CoursesTable";
+
+function MyCourses(){
+    const { token } = useSelector((state) => state.auth);
+    const { navigate } = useNavigate();
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const result = await fetchInstructorCourses(token)
+            if(token){
+                setCourses(result);
+            }
+        }
+        fetchCourses()
+    }, [])
+
+    return(
+        <div>
+            <div className="mb-14 flex items-center justify-between">
+                <h1 className="text-3xl font-medium text-richblack-5">My courses</h1>
+                <IconBtn 
+                    text={"Add Course"}
+                    onClick={() => navigate("/dashboard/add-course")}
+                >
+                    <VscAdd/>
+                </IconBtn>
+            </div>
+            {courses && <CoursesTable courses={courses} setCourses={setCourses}/>}
+        </div>
+    )
+}
+
+export {
+    MyCourses
+}
