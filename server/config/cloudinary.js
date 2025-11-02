3b9b60efef13b146a6a478f5a20ad031e9cc704a
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-02 19:35:34 +0530

    Corrected the code in database file

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -17,3 +17,4 @@ function cloudinaryConnect(){
 export {
     cloudinaryConnect
 }
+
--- server/config/database.js
+++ server/config/database.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
-export const connect = () => { 
-    mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log("Db Connection success"))
-    .catch((err) => {
-        console.log(err);
+export const connect = async () => { 
+    
+    try{
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    }catch(e){
+        console.log(e);
         process.exit(1);
-    })
+    }
 }
