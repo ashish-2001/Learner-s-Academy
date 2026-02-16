3a0478a97d62056afc98e13d3f77a92a3c9cdb04
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-16 14:36:28 +0530

    Added timeline section

--- src/components/core/HomePage/TimelineSection.jsx
+++ src/components/core/HomePage/TimelineSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Logo1 from "../../../assets/timeLineLogo/Logo1.svg"
+import Logo1 from "../../../assets/timeLineLogo/logo1.svg"
 import Logo2 from "../../../assets/TimeLineLogo/Logo2.svg"
 import Logo3 from "../../../assets/TimeLineLogo/Logo3.svg"
 import Logo4 from "../../../assets/TimeLineLogo/Logo4.svg"
