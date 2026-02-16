0902539ea91d065758568543186724bd92b80b4c
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-16 13:37:25 +0530

    Added my profile

--- src/components/core/Dashboard/MyProfile.jsx
+++ src/components/core/Dashboard/MyProfile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { IconBtn } from "../../Common/IconBtn"
+import { IconBtn } from "../../common/IconBtn"
 
 const MyProfile = () => {
 
