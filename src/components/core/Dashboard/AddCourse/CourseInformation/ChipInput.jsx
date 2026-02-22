50ff946b43962ce20ab444ebd1c9ee11a63207a4
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-22 16:03:40 +0530

    Added chip input

--- src/components/core/Dashboard/AddCourse/CourseInformation/ChipInput.jsx
+++ src/components/core/Dashboard/AddCourse/CourseInformation/ChipInput.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 import { FaTimes } from 'react-icons/fa';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
 
 const ChipInput = ({ name, label, register, errors, setValue }) => {
-    const [tags, setTags] = useState([])
+    const [tags, setTags] = useState([]);
     const {editCourse, course} = useSelector((state) => state.course);
 
     
@@ -24,62 +25,61 @@ const ChipInput = ({ name, label, register, errors, setValue }) => {
                 setTags(parsedTags);
                 setValue(name, parsedTags);
             } catch(error){
-                console.error("Invalid tag format", error.message);
+                toast.error("Invalid tag format", error.message);
                 setTags([]);
                 setValue(name, []);
-            }
-            
-        }
-    },[])
+            };
+        };
+    }, []);
 
 return (
-    <div>
-        <label className='text-sm text-[#F1F2FF]' htmlFor={name}>{label}<sup className='text-[#EF476F]'>*</sup></label>
-        <div className='flex flex-wrap gap-2 m-2'>
-            {
-                tags.map((tag, index) => (
-                    <div key={index} className='m-1 flex items-center rounded-full bg-[#9E8006] px-2 py-1 text-sm text-[#F1F2FF]'>
-                        <span className='text-[#F1F2FF]'>{tag}</span>
-                        <button
-                            type='button'
-                            onClick={() => {
-                                const updatedTags = [...tags];
-                                updatedTags.splice(index, 1);
-                                setTags(updatedTags);
-                                setValue(name, updatedTags);
-                            }}
-                            className='ml-2 text-[#F1F2FF]'
-                        >
-                            <FaTimes/>
-                        </button>
-                        </div>
-                ))
-            }
-    </div>
-    <input
-        type='text'
-        id={name}
-        placeholder='Press Enter or , to add a tag'
-        className='rounded-lg bg- p-3 text-[16px] leading-[24px] text-[#F1F2FF] shadow-[0_1px_0_0] shadow-white/50 placeholder:text-[#6E727F] focus:outline-none w-full'
-        onKeyDown={(e) => {
-            if(e.key === 'Enter' || e.key === ',') {
-                e.preventDefault();
-                if(e.target.value) {
-                    setTags([...tags, e.target.value]);
-                    setValue(name, [...tags, e.target.value]);
-                    e.target.value = "";
+        <div>
+            <label className='text-sm text-[#F1F2FF]' htmlFor={name}>{label}<sup className='text-[#EF476F]'>*</sup></label>
+            <div className='flex flex-wrap gap-2 m-2'>
+                {
+                    tags.map((tag, index) => (
+                        <div key={index} className='m-1 flex items-center rounded-full bg-[#9E8006] px-2 py-1 text-sm text-[#F1F2FF]'>
+                            <span className='text-[#F1F2FF]'>{tag}</span>
+                            <button
+                                type='button'
+                                onClick={() => {
+                                    const updatedTags = [...tags];
+                                    updatedTags.splice(index, 1);
+                                    setTags(updatedTags);
+                                    setValue(name, updatedTags);
+                                }}
+                                className='ml-2 text-[#F1F2FF]'
+                            >
+                                <FaTimes/>
+                            </button>
+                            </div>
+                    ))
                 }
-            }
-        }}
-    />
-    {
-        errors[name] && <span className='text-xs text-[#EF476F]'>Tags are required</span>
-        
-    }
-    </div>
-)
-}
+        </div>
+        <input
+            type='text'
+            id={name}
+            placeholder='Press Enter or , to add a tag'
+            className='rounded-lg bg- p-3 text-[16px] leading-[24px] text-[#F1F2FF] shadow-[0_1px_0_0] shadow-white/50 placeholder:text-[#6E727F] focus:outline-none w-full'
+            onKeyDown={(e) => {
+                if(e.key === 'Enter' || e.key === ',') {
+                    e.preventDefault();
+                    if(e.target.value) {
+                        setTags([...tags, e.target.value]);
+                        setValue(name, [...tags, e.target.value]);
+                        e.target.value = "";
+                    }
+                }
+            }}
+        />
+        {
+            errors[name] && <span className='text-xs text-[#EF476F]'>Tags are required</span>
+            
+        }
+        </div>
+    );
+};
 
 export {
     ChipInput
-}
+};
