7fb25bae7fab622ed826b63632a55a3a5c5d41c8
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-23 00:16:17 +0530

    Added reset password

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -9,25 +9,24 @@ async function resetPasswordToken(req, res){
         const email = req.body.email;
         const user = await User.findOne({
             email: email
-        }) 
+        });
 
         if(!user){
             return res.status(404).json({
                 success: false,
                 message: `This email: ${email} is not registered with us. Enter a valid email.`
-            })
-
-        }
+            });
+        };
 
         const token = crypto.randomBytes(20).toString("hex");
 
         const updatedDetails = await User.findOneAndUpdate({
-            email: email
-        }, {
-            token: token,
-            resetPasswordExpires: Date.now() + 3600000
-        }, {
-            new: true
+                email: email
+            }, {
+                token: token,
+                resetPasswordExpires: Date.now() + 3600000
+            }, {
+                new: true
         });
 
         console.log("Details:", updatedDetails);
@@ -38,22 +37,20 @@ async function resetPasswordToken(req, res){
             email,
             "Password Reset",
             `Your Link for email verification is ${url}. Please click this url to reset password`
-        )
+        );
 
         return res.status(200).json({
             success: true,
             message: "Email sent successfully. Please check email inbox to continue"
-        })
-    }
-    catch(e){
+        });
+    } catch(e){
         return res.status(500).json({
             error: e.message,
             success: false,
             message: "Some error occurred while sending the reset message"
         });
-
-    }
-}
+    };
+};
 
 const resetPasswordValidator1 = z.object({
         password: z.string().min(6, "Password must be at least 6 characters long"),
@@ -72,61 +69,61 @@ async function resetPassword(req, res){
             return res.status(400).json({
                 success: false,
                 message: "Password and Confirm password does not match"
-            })
-        }
-
+            });
+        };
         const { password, confirmPassword, token } = parsedResult.data;
-
-            if(confirmPassword !== password){
-                return res.status(400).json({
-                    success: false,
-                    message: "Password and Confirm password does not match"
-                })
-            }
+        if(confirmPassword !== password){
+            return res.status(400).json({
+                success: false,
+                message: "Password and Confirm password does not match"
+            });
+        };
 
         const userDetails = await User.findOne({
             token: token
-        })
+        });
 
         if(!userDetails){
             return res.status(404).json({
                 success: false,
                 message: "Token is invalid"
-            })
-        }
+            });
+        };
 
         if(!(userDetails.resetPasswordExpires > Date.now())){
             return res.status(403).json({
                 success: false,
                 message: "Token is expired, please regenerate your token"
-            })
-        }
+            });
+        };
 
         const encryptedPassword = await bcrypt.hash(password, 10);
 
         await User.findOneAndUpdate({
-            token: token
-        }, {
-            password: encryptedPassword
-        }, {
-            new: true
-        })
+                token: token
+            }, 
+            {
+                password: encryptedPassword
+            }, 
+            {
+                new: true
+            }
+        );
 
         return res.status(200).json({
             success: true,
             message: "Password reset successful"
-        })
-    }
-    catch(e){
+        });
+    } catch(e){
         return res.status(400).json({
             success: true,
             message: "Some error in updating the password",
             errors: e.message
-        })
-    }
-}
+        });
+    };
+};
 
 export {
     resetPasswordToken,
     resetPassword
-}
+};
