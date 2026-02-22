7d0ebaf2a13fe1a071d2d0db285c2da4ccad4f23
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-22 15:30:13 +0530

    Added auth api

--- src/services/operations/authAPI.jsx
+++ src/services/operations/authAPI.jsx
@@ -93,43 +93,43 @@ function signUp(
 
 function login(email, password, navigate){
     return async (dispatch)=> {
-        const toastId = toast.loading("Loading....")
-        dispatch(setLoading(true))
+
+        const toastId = toast.loading("Loading....");
+        dispatch(setLoading(true));
+        
         try{
             const response = await apiConnector("POST", LOGIN_API, {
                 email,
                 password
-            })
+            });
 
-            console.log("LOGIN API RESPONSE..........", response)
             if(!response.data.success){
                 throw new Error(response.data.message)
-            }
+            };
 
-            toast.success("Login successful")
-            dispatch(setToken(response.data.token))
+            toast.success("Login successful");
+            dispatch(setToken(response.data.token));
 
             const userImage = response.data?.user?.image
             ? response.data.user.image
             : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.user.firstName} ${response.data.user.lastName}`
-            dispatch(setUser({ ...response.data.user, image:userImage }))
-            localStorage.setItem("token", JSON.stringify(response.data.token))
+            dispatch(setUser({ ...response.data.user, image:userImage }));
+            localStorage.setItem("token", JSON.stringify(response.data.token));
 
             if(response.data.user.accountType === "Instructor" && !response.data.user.approved){
                 toast("Your account is pending for approval by admin");
                 navigate("/");
             } else{
-                navigate("/dashboard/my-profile")
-            }
+                navigate("/dashboard/my-profile");
+            };
         } catch(error){
-            console.log("LOGIN API ERROR...........", error)
-            const errorMessage = error?.response?.data?.message || "Login Failed"
-            toast.error(errorMessage)
+            const errorMessage = error?.response?.data?.message || "Login Failed";
+            toast.error(errorMessage);
         }
-        dispatch(setLoading(false))
-        toast.dismiss(toastId)
-    }
-}
+        dispatch(setLoading(false));
+        toast.dismiss(toastId);
+    };
+};
 
 function getPasswordResetToken(email, setEmailSent){
     return async (dispatch) => {
@@ -138,29 +138,27 @@ function getPasswordResetToken(email, setEmailSent){
         try{
             const response = await apiConnector("POST", RESETPASSTOKEN_API, {
                 email
-            })
-
-            console.log("RESETPASSTOKEN RESPONSE............", response)
+            });
 
             if(!response.data.success){
                 throw new Error(response.data.message)
-            }
-            toast.success("Reset Email Sent")
+            };
+            toast.success("Reset Email Sent");
             setEmailSent(true);
 
         } catch(error){
-            console.log("RESETPASSTOKEN ERROR............", error)
-            toast.error("Failed to send reset email")
+            console.log("RESETPASSTOKEN ERROR............", error);
+            toast.error("Failed to send reset email");
         }
-        toast.dismiss(toastId)
-        dispatch(setLoading(false))
+        toast.dismiss(toastId);
+        dispatch(setLoading(false));
     }
 }
 
 function resetPassword(password, confirmPassword, token, setResetComplete){
     return async (dispatch) => {
-        const toastId = toast.loading("Loading...")
-        dispatch(setLoading(true))
+        const toastId = toast.loading("Loading...");
+        dispatch(setLoading(true));
         try{
             const response = await apiConnector("POST", RESETPASSWORD_API, {
                 password,
@@ -168,11 +166,9 @@ function resetPassword(password, confirmPassword, token, setResetComplete){
                 token
             });
 
-            console.log("RESETPASSWORD RESPONSE........", response);
-
             if(!response.data.success){
                 throw new Error(response.data.message)
-            }
+            };
 
             toast.success("Password Reset Successfully")
             setResetComplete(true);
@@ -180,24 +176,24 @@ function resetPassword(password, confirmPassword, token, setResetComplete){
         }catch(error){
             console.log("RESETPASSWORD ERROR..........", error)
             toast.error("Failed to reset password")
-        }
+        };
 
         toast.dismiss(toastId)
         dispatch(setLoading(false))
-    }
-}
+    };
+};
 
 function logout(navigate){
     return (dispatch) => {
-        dispatch(setToken(null))
-        dispatch(setUser(null))
-        dispatch(resetCart())
-        localStorage.removeItem("item")
-        localStorage.removeItem("user")
-        toast.success("Logged Out")
-        navigate("/")
-    }
-}
+        dispatch(setToken(null));
+        dispatch(setUser(null));
+        dispatch(resetCart());
+        localStorage.removeItem("item");
+        localStorage.removeItem("user");
+        toast.success("Logged Out");
+        navigate("/");
+    };
+};
 
 function forgotPassword(email, setEmailSent){
     return async (dispatch) => {
@@ -207,20 +203,20 @@ function forgotPassword(email, setEmailSent){
                 email
             });
 
-            console.log("Forgot password response", response);
-
             if(!response.data.success){
                 toast.error(response.data.message);
                 throw new Error(response.data.message);
-            }
+            };
+
             toast.success("Reset Email Sent");
             setEmailSent(true);
+
         } catch(error){
             console.log("Forgot Password Error", error);
         }
         dispatch(setLoading(false));
-    }
-}
+    };
+};
 
 
 export {
@@ -231,4 +227,4 @@ export {
     resetPassword,
     forgotPassword,
     logout
-}
+};
