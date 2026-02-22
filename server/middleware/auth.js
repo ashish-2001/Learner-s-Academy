910fd80a61c433ed2f78f06712e000116e8d0467
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-23 00:26:47 +0530

    Added auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,39 +3,35 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function auth(req, res, next){
+
     try{
-
         let token = req.cookies.token || req.header("Authorization")?.replace("Bearer ", "") || req.body.token;
-
         if(!token){
             return res.status(401).json({
                 success: false,
                 message: "Token is Missing"
-            })
-        }
-
-        try{
-            
+            });
+        };
+        try{  
             const decode = jwt.verify(token, process.env.JWT_SECRET);
             console.log("decode", decode);
             req.user = decode;
 
         } catch(e){
-                return res.status(401).json({
-                    success: false,
-                    message: ("Token is invalid", e.message)
-                })
-        }
+            return res.status(401).json({
+                success: false,
+                message: ("Token is invalid", e.message)
+            });
+        };
         next();
-    }
-    catch(e){
+    } catch(e){
         return res.status(401).json({
             success: false,
             message: "Something went wrong while validating the token",
             error: e.message
-        })
-    }
-}
+        });
+    };
+};
 
 async function isStudent(req, res, next){
     try{
@@ -43,19 +39,17 @@ async function isStudent(req, res, next){
             return res.status(403).json({
                 success: false,
                 message: "This is a protected route for students only"
-            })
-        }
-
+            });
+        };
         next();
-    }
-    catch(e){
+    } catch(e){
         return res.status(500).json({
             success: false,
             message: "Students role can't be verified",
             error: e.message
-        })
-    }
-}
+        });
+    };
+};
 
 
 async function isInstructor(req, res, next){
@@ -65,39 +59,34 @@ async function isInstructor(req, res, next){
             return res.status(401).json({
                 success: false,
                 message: "This is a protected route for Instructor only"
-            })
-        }
-
+            });
+        };
         next();
-    }
-    catch(e){
+    } catch(e){
         return res.status(500).json({
             success: false,
             message: "instructor role can't be verified",
             error: e.message
-        })
-    }
-}
+        });
+    };
+};
 
 async function isAdmin(req, res, next){
     try{
-
         if(req.user.accountType !== "Admin"){
             return res.status(401).json({
                 success: false,
                 message: "This is a protected route for Admin ony"
-            })
-        }
-
+            });
+        };
         next();
-    }
-    catch(e){
+    } catch(e){
         return res.status(500).json({
             success: false,
             message: "Admin role can't be verified",
             error: e.message
-        })
-    }
+        });
+    };
 };
 
 
@@ -106,4 +95,4 @@ export {
     isStudent,
     isAdmin,
     isInstructor
-}
+};
