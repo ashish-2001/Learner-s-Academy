04e962d6ca542a0b7aaaaac2b6d6eff2634459e3
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-22 15:46:12 +0530

    Added format date

--- src/services/formatDate.jsx
+++ src/services/formatDate.jsx
@@ -1,5 +1,5 @@
 function formatDate(dateString){
-    const options = { year: "numeric", month: "long", day: "numeric"}
+    const options = { year: "numeric", month: "long", day: "numeric"};
     const date = new Date(dateString);
 
     const formattedDate = date.toLocaleDateString("en-US", options);
@@ -8,12 +8,12 @@ function formatDate(dateString){
     const minutes = date.getMinutes();
     const period = hour >= 12 ? "PM" : "AM";
 
-    const formattedTime = `${hour % 12} : ${minutes.toString().padStart(2, "0")} ${period}`
+    const formattedTime = `${hour % 12} : ${minutes.toString().padStart(2, "0")} ${period}`;
 
-        return `${formattedDate} | ${formattedTime}`
-}
+    return `${formattedDate} | ${formattedTime}`;
+};
 
 
 export {
     formatDate
-}
+};
