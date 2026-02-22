bf05fdef40fbd3167473194aed0a2346086f8097
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-23 00:38:33 +0530

    Added image uploader

--- server/utils/ImageUploader.js
+++ server/utils/ImageUploader.js
@@ -3,19 +3,18 @@ import { v2 as cloudinary } from "cloudinary";
 async function uploadImageToCloudinary(file, folder, height, quality){
 
     const options = { folder };
-
     if(height){
         options.height = height;
-    }
+    };
     if(quality){
         options.quality = quality;
-    }
+    };
     options.resource_type = "auto";
 
-    return await cloudinary.uploader.upload(file.tempFilePath, options)
-}
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+};
 
 
 export {
     uploadImageToCloudinary
-}
+};
