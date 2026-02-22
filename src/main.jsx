82568c69199a2def9e176a2c0333da3f9d43f084
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-22 15:01:25 +0530

    Fixed some bugs in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
-import { App } from './App'
-import "./App.css"
-import { BrowserRouter } from 'react-router-dom'
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from "react-redux"
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import "./App.css";
+import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from "react-redux";
 import { rootReducer } from './reducer/index';
 import { Toaster } from 'react-hot-toast';
 import { ScrollToTop } from './components/ScrollToTop';
 
 const store = configureStore({
     reducer: rootReducer
-})
+});
 
 createRoot(document.getElementById('root')).render(
         <Provider store={store}>
