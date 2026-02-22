e6a8ecb9d610a4ff083657448bc0d0fdf731485a
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-22 15:40:18 +0530

    Added profile api

--- src/services/operations/profileAPI.jsx
+++ src/services/operations/profileAPI.jsx
@@ -15,25 +15,21 @@ async function getUserCourses(token, dispatch){
                 }
         );
 
-            console.log("AFTER CALLING BACKEND API FOR ENROLLED COURSES");
-
-            if(!response.data.success){
-                throw new Error(response.data.message);
-            }
-            result = response.data.data;
-        } catch(error){
-            console.log("Get user details api error.....", error)
-            toast.error("Could not get Enrolled courses");
+        if(!response.data.success){
+            throw new Error(response.data.message);
         }
-        dispatch(setProgress(100));
-        return result;
-}
+        result = response.data.data;
+    } catch(error){
+        toast.error("Could not get Enrolled courses", error.message);
+    }
+    dispatch(setProgress(100));
+    return result;
+};
 
 async function getProfilePicture(token, profilePicture){
     const toastId = toast.loading("Loading...");
     try{
         const formData = new FormData();
-        console.log("Profile picture:", profilePicture);
         formData.append("displayPicture", profilePicture);
 
         const response = await apiConnector("PUT", settingsEndpoints.UPDATE_DISPLAY_PICTURE_API, formData, 
@@ -41,24 +37,19 @@ async function getProfilePicture(token, profilePicture){
                 Authorization: `Bearer ${token}` 
             }
     );
-        console.log("UPDATE DISPLAY PICTURE API RESPONSE.............", response);
         if(!response.data.success){
             throw new Error(response.data.message);
-        }
+        };
         toast.success("Profile picture uploaded successfully");
         const imageUrl = response.data.data.image;
         localStorage.setItem("user", JSON.stringify({ ...JSON.parse(localStorage.getItem("user")), image: imageUrl }));
-        console.log(JSON.parse(localStorage.getItem("user")).image);
     } catch(error){
-        console.log("UPDATE DISPLAY PICTURE API ERROR.................", error);
         toast.error(error?.response?.data?.message);
-    }
+    };
     toast.dismiss(toastId);
-}
+};
 
 async function updateAdditionalDetails(token, additionalDetails){
-
-    console.log("Additional details", additionalDetails);
 
     const {
         firstName, 
@@ -69,22 +60,20 @@ async function updateAdditionalDetails(token, additionalDetails){
         about
     } = additionalDetails;
 
-    console.log("Additional details:", additionalDetails);
-
     const toastId = toast.loading("Updating Profile...");
 
     try{
 
         const response = await apiConnector("PUT", settingsEndpoints.UPDATE_PROFILE_API, { firstName, lastName, dateOfBirth, gender, contactNumber, about }, 
-            {
-                Authorization: `Bearer ${token}` 
-            }
-    )
-        console.log("UPDATE ADDITIONAL DETAILS API RESPONSE...............", response)
+                {
+                    Authorization: `Bearer ${token}` 
+                }
+            );
+        
         
         if(!response.data.success){
             throw new Error(response.data.message);
-        }
+        };
 
         toast.success("Additional Details updated successfully");
 
@@ -101,23 +90,22 @@ async function updateAdditionalDetails(token, additionalDetails){
                 about: about || user.additionalDetails?.about,
                 gender: gender || user.additionalDetails?.gender
             }
-        }
+        };
     
         localStorage.setItem("user", JSON.stringify(updatedUser));
 
         return updatedUser;
 
     } catch(error){
-        console.log("UPDATE ADDITIONAL DETAILS API ERROR...............", error);
         toast.error(error.response.data.message);
     } finally{
         toast.dismiss(toastId);
-    }
-}
+    };
+};
 
 async function updatePassword(token, password){
     const { oldPassword, newPassword, confirmPassword: confirmPassword } = password;
-    console.log("password", password);
+    
     const toastId = toast.loading("Updating Password...");
 
     try{
@@ -126,69 +114,60 @@ async function updatePassword(token, password){
                 Authorization: `Bearer ${token}` 
             }
     );
-        console.log("UPDATE PASSWORD API RESPONSE...............", response);
         if(!response.data.success){
             throw new Error(response.data.message);
         }
         toast.success("Password updated successfully");
     }catch(error){
-        console.log("UPDATE PASSWORD API ERROR..............", error);
         toast.error(error?.response?.data?.message);
     }
     toast.dismiss(toastId);
-}
+};
 
 async function deleteAccount(token, dispatch, navigate){
     const toastId = toast.loading("Deleting Account...");
     try{
         const response = await apiConnector("DELETE", settingsEndpoints.DELETE_PROFILE_API, null, 
-            {
-                Authorization: `Bearer ${token}` 
-            }
-    );
-        console.log("DELETE ACCOUNT API RESPONSE..............", response);
-        if(!response.data.success){
-            throw new Error(response.data.message);
+        {
+            Authorization: `Bearer ${token}` 
         }
-        toast.success("Account Deleted successfully");
-        dispatch(logout(navigate));
+    );
+    if(!response.data.success){
+        throw new Error(response.data.message);
+    }
+    toast.success("Account Deleted successfully");
+    dispatch(logout(navigate));
     } catch(error){
-        console.log("DELETE ACCOUNT API ERROR.................", error);
         toast.error(error?.response?.data?.message);
     } finally {
         toast.dismiss(toastId);
-    }
-}
+    };
+};
 
 async function getInstructorDashboard(token, dispatch){
 
-        dispatch(setProgress(30));
-        let result = [];
+    dispatch(setProgress(30));
+    let result = [];
 
-        try{
-            console.log("BEFORE CALLING BACKEND API FOR INSTRUCTOR DASHBOARD");
-            const response = await apiConnector("GET", profileEndpoints.GET_ALL_INSTRUCTOR_DASHBOARD_DETAILS_API, null, 
-                {
-                    Authorization: `Bearer ${token}` 
-                }
-        );
-            console.log("AFTER CALLING BACKEND API FOR INSTRUCTOR DASHBOARD");
-
-            if(!response.data.success){
-                throw new Error(response.data.message);
+    try{
+        const response = await apiConnector("GET", profileEndpoints.GET_ALL_INSTRUCTOR_DASHBOARD_DETAILS_API, null, 
+            {
+                Authorization: `Bearer ${token}` 
             }
+        );
 
-            result = response.data.data;
-            console.log(result);
-        }catch(error){
-            console.log("GET INSTRUCTOR DASHBOARD API ERROR................", error);
-            toast.error(error?.response?.data?.message);
-        } finally {
-            dispatch(setProgress(100));
+        if(!response.data.success){
+            throw new Error(response.data.message);
         }
-        
-        return result;
+        result = response.data.data;
+    }catch(error){
+        toast.error(error?.response?.data?.message);
+    } finally {
+        dispatch(setProgress(100));
     }
+    
+    return result;
+};
 
 export {
     updateAdditionalDetails,
@@ -197,4 +176,4 @@ export {
     getInstructorDashboard,
     getUserCourses,
     deleteAccount
-}
+};
