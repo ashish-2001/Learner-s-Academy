8b1d07e167ca142cd040f5581b0499caafc41dd9
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-22 14:28:42 +0530

    Added avg rating

--- src/utils/avgRating.jsx
+++ src/utils/avgRating.jsx
@@ -9,8 +9,8 @@ function GetAvgRating(ratingArr){
     const multiplier = Math.pow(10, 1)
     const avgReviewCount = Math.round((totalReviewCount / ratingArr?.length) * multiplier) / multiplier;
     return avgReviewCount
-}
+};
 
 export {
     GetAvgRating
-}
+};
