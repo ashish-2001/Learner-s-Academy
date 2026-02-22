a60beffd989c830e8898098096e148d642b02344
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-22 23:47:24 +0530

    Added database

--- server/config/database.js
+++ server/config/database.js
@@ -6,11 +6,9 @@ export const connect = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
-        })
-
-        console.log("Database connected!");
+        });
     }catch(e){
         console.log(e);
         process.exit(1);
-    }
-}
+    };
+};
