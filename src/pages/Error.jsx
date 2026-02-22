8780e07a0162ff887398b5ee768abf6ca6c17797
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-22 22:48:52 +0530

    Added error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,13 @@
-import React from "react"
+import React from "react";
 
 function Error(){
     return(
         <div className="flex w-screen h-screen justify-center items-center text-white text-3xl">
             Error 404 - Page not Found
         </div>
-    )
-}
+    );
+};
 
 export {
     Error
-}
+};
