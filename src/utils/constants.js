25fd5699e5b210ad1a10250287ee917eab21beda
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-22 15:02:04 +0530

    Added constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,14 +2,14 @@ const ACCOUNT_TYPE = {
     STUDENT: "Student",
     INSTRUCTOR: "Instructor",
     ADMIN: "Admin"
-}
+};
 
 const COURSE_STATUS = {
     DRAFT: "Draft",
     PUBLISHED: "Published"
-}
+};
 
 export {
     ACCOUNT_TYPE,
     COURSE_STATUS
-}
+};
