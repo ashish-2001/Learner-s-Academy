00c19e19626d584b4ed7fd0f2e1d68fd41542800
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-22 15:45:16 +0530

    Added apis

--- src/services/apis.jsx
+++ src/services/apis.jsx
@@ -7,19 +7,19 @@ const endpoints = {
     LOGIN_API: BASE_URL + "/auth/login",
     RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
     RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
-}
+};
 
 const profileEndpoints = {
     GET_USER_DETAILS_API: BASE_URL + "/profile/getUserDetails",
     GET_USER_ENROLLED_COURSES_API: BASE_URL + "/profile/getEnrolledCourses",
     GET_ALL_INSTRUCTOR_DASHBOARD_DETAILS_API: BASE_URL + "/profile/getInstructorDashboardDetails",
-}
+};
 
 const studentEndpoints = {
     COURSE_PAYMENT_API: BASE_URL + "/payment/capturePayment",
     COURSE_VERIFY_API: BASE_URL + "/payment/verifyPayment",
     SEND_PAYMENT_SUCCESS_EMAIL_API: BASE_URL + "/payment/sendPaymentSuccessEmail",
-}
+};
 
 const courseEndpoints = {
     GET_ALL_COURSE_API: BASE_URL + "/course/getAllCourses",
@@ -41,30 +41,30 @@ const courseEndpoints = {
     ADD_COURSE_TO_CATEGORY_API: BASE_URL + "/course/addCourseToCategory",
     SEARCH_COURSES_API: BASE_URL + "/course/searchCourse",
     CREATE_CATEGORY_API: BASE_URL + "/course/createCategory"
-}
+};
 
 const ratingsEndpoints = {
     REVIEWS_DETAILS_API: BASE_URL + "/course/getReviews",
-}
+};
 
 const categories = {
     CATEGORIES_API: BASE_URL + "/course/showAllCategories",
-}
+};
 
 const catalogData = {
     CATALOGPAGEDATA_API: BASE_URL + "/course/getCategoryPageDetails",
-}
+};
 
 const contactUsEndpoint = {
     CONTACT_US_API: BASE_URL + "/contact/contactUs",
-}
+};
 
 const settingsEndpoints = {
     UPDATE_DISPLAY_PICTURE_API: BASE_URL + "/profile/updateDisplayPicture",
     UPDATE_PROFILE_API: BASE_URL + "/profile/updateProfile",
     CHANGE_PASSWORD_API: BASE_URL + "/auth/changepassword",
     DELETE_PROFILE_API: BASE_URL + "/profile/deleteProfile",
-}
+};
 
 
 export {
@@ -77,4 +77,4 @@ export {
     catalogData,
     contactUsEndpoint,
     settingsEndpoints
-}
+};
