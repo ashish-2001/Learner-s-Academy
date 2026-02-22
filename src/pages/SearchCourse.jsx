7150c59cda0d275667edfe74aaf3fed2ca0778b8
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-22 23:02:50 +0530

    Added search course

--- src/pages/SearchCourse.jsx
+++ src/pages/SearchCourse.jsx
@@ -22,72 +22,67 @@ const SearchCourse = () => {
         setSearchResults(res || []);
         setLoading(false);
         console.log(res);
-    }
+    };
 
     useEffect(() => {
         fetchSearchResults();
-    }, [searchQuery])
-
+    }, [searchQuery]);
 
   return (
     <div>
-        <div className='mx-auto flex min-h-[260px]  flex-col justify-center gap-4 p-5 bg-[#161D29] '>
+      <div className='mx-auto flex min-h-[260px]  flex-col justify-center gap-4 p-5 bg-[#161D29] '>
         <p className='text-sm text-[#838894]'>Home / Search / <span className='text-[#FFE83D]'>{searchQuery}</span> </p>
         <p className='text-3xl text-[#F1F2FF]'>Search Results for {searchQuery}</p>
         <p className='max-w-[870px] text-[#999DAA]'>
             {searchResults?.length} results found for {searchQuery}
         </p>
-        </div>
+      </div>
         {
-            loading ? (
-                <div className='flex gap-6 flex-wrap justify-evenly overflow-hidden m-9'>
+          loading ? (
+          <div className='flex gap-6 flex-wrap justify-evenly overflow-hidden m-9'>
+            <SkeletonTheme baseColor="#2C333F" highlightColor="#161D29">
+          <div className=''>
+            <Skeleton  className="md:h-[200px] lg:w-[400px] h-[100px] w-[200px] rounded-xl" />
+            <Skeleton className=" md:h-[20px] w-[70px] rounded-md" />
+            <Skeleton className="md:h-[20px] md:w-[400px] rounded-md" />
+            <Skeleton className="md:h-[20px] md:w-[400px] rounded-md"/>
+          </div>
+            </SkeletonTheme>
                 <SkeletonTheme baseColor="#2C333F" highlightColor="#161D29">
-                <div className=''>
-                  <Skeleton  className="md:h-[200px] lg:w-[400px] h-[100px] w-[200px] rounded-xl" />
-                  <Skeleton className=" md:h-[20px] w-[70px] rounded-md" />
-                  <Skeleton className="md:h-[20px] md:w-[400px] rounded-md" />
-                  <Skeleton className="md:h-[20px] md:w-[400px] rounded-md"/>
-                </div>
-              </SkeletonTheme>
+              <div className=''>
+                <Skeleton  className="md:h-[200px] lg:w-[400px] h-[100px] w-[200px] rounded-xl" />
+                <Skeleton className=" md:h-[20px] w-[70px] rounded-md" />
+                <Skeleton className="md:h-[20px] md:w-[400px] rounded-md" />
+                <Skeleton className="md:h-[20px] md:w-[400px] rounded-md"/>
+              </div>
+            </SkeletonTheme>
                 <SkeletonTheme baseColor="#2C333F" highlightColor="#161D29">
-                <div className=''>
-                  <Skeleton  className="md:h-[200px] lg:w-[400px] h-[100px] w-[200px] rounded-xl" />
-                  <Skeleton className=" md:h-[20px] w-[70px] rounded-md" />
-                  <Skeleton className="md:h-[20px] md:w-[400px] rounded-md" />
-                  <Skeleton className="md:h-[20px] md:w-[400px] rounded-md"/>
-                </div>
-              </SkeletonTheme>
-                <SkeletonTheme baseColor="#2C333F" highlightColor="#161D29">
-                <div className=''>
-                  <Skeleton  className="md:h-[200px] lg:w-[400px] h-[100px] w-[200px] rounded-xl" />
-                  <Skeleton className=" md:h-[20px] w-[70px] rounded-md" />
-                  <Skeleton className="md:h-[20px] md:w-[400px] rounded-md" />
-                  <Skeleton className="md:h-[20px] md:w-[400px] rounded-md"/>
-                </div>
-              </SkeletonTheme>
-                </div>
-            ):
-
-            searchResults?.length===0 ? (<div className='mx-auto flex min-h-[500px] items-center  flex-col justify-center gap-4 p-5 bg-[#000814] '>
-                <p className='text-3xl text-[#F1F2FF]'>No Results Found</p>
-                <HiOutlineEmojiSad className=' text-[#AFB2BF] text-[100px]' />
-            </div>):
-            (
-                <div className='mx-auto flex flex-wrap p-5 gap-4 justify-evenly m-5'>
+              <div className=''>
+                <Skeleton  className="md:h-[200px] lg:w-[400px] h-[100px] w-[200px] rounded-xl" />
+                <Skeleton className=" md:h-[20px] w-[70px] rounded-md" />
+                <Skeleton className="md:h-[20px] md:w-[400px] rounded-md" />
+                <Skeleton className="md:h-[20px] md:w-[400px] rounded-md"/>
+              </div>
+            </SkeletonTheme>
+          </div>
+        ): searchResults?.length===0 ? (
+            <div className='mx-auto flex min-h-[500px] items-center  flex-col justify-center gap-4 p-5 bg-[#000814] '>
+              <p className='text-3xl text-[#F1F2FF]'>No Results Found</p>
+              <HiOutlineEmojiSad className=' text-[#AFB2BF] text-[100px]' />
+            </div> ) : (
+              <div className='mx-auto flex flex-wrap p-5 gap-4 justify-evenly m-5'>
                 {searchResults?.map((item)=>(
                     <div className='flex flex-col gap-4' key={item._id}>
                     <Course_Card course={item} Height={"lg:h-[250px] h-[100px]"} />
                     </div>
                 ))}
-                </div>
+              </div>
             )
-        }
-       
-            
+        }   
     </div>
-  )
-}
+  );
+};
 
 export {
   SearchCourse
-}
+};
