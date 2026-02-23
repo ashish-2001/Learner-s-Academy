c8b9ef829afcfcc6d69d30c3bfbf993355599412
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-23 13:35:53 +0530

    Added tab

--- src/components/common/Tab.jsx
+++ src/components/common/Tab.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Tab({ tabData, field, setField }){
 
-    return(
+    return (
         <div style={{ boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)"}} className="flex bg-[#161D29] p-1 gap-x-1 my-6 rounded-full max-w-max">
             {tabData.map((tab) => {
                 return(
