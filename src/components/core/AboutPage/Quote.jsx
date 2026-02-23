f085ab6c87ab2b734aea470f6242a4d9ddc9f202
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-23 14:49:13 +0530

    Added quote

--- src/components/core/AboutPage/Quote.jsx
+++ src/components/core/AboutPage/Quote.jsx
@@ -5,8 +5,8 @@ import { HighLightText } from "../../../components/core/HomePage/HighLightText";
 function Quote(){
     return (
         <div className="text-xl md:text-4xl font-semibold mx-auto py-5 pb-20 text-center text-white">
-            We are passionate about revolutionizing the way we learn. Our
-            innovative platform <HighLightText text={"combines technology"}/>, {" "}
+            We are passionate about revolutionizing the way we learn. Our innovative platform 
+                <HighLightText text={"combines technology"}/>, {" "}
             <span className="bg-gradient-to-b from-[#FF512F] to-[#F09819] text-[#ffff00] bg-clip-text font-bold">
                 {" "}
                 expertise
