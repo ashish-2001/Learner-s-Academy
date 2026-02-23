7f14b73ac43ba9123fa05fff8c6b58718bb94256
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-23 15:19:22 +0530

    Added open route

--- src/components/core/auth/OpenRoute.jsx
+++ src/components/core/auth/OpenRoute.jsx
@@ -3,14 +3,15 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 function OpenRoute({ children }){
+
     const { token } = useSelector((state) => state.auth);
 
     if(token === null){
         return children;
-    }
-    else{
+    } else{
         return <Navigate to="/dashboard/my-profile"/>
     }
+
 };
 
 export {
