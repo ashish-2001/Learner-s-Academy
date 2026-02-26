fb35ba021e55ca6398e5bf5d15864db8933f64e8
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-26 14:59:37 +0530

    Added Learning grid

--- src/components/core/AboutPage/LearningGrid.jsx
+++ src/components/core/AboutPage/LearningGrid.jsx
@@ -45,40 +45,39 @@ const LearningGridArray = [
 
 function LearningGrid(){
 
-    return(
-        <div className="grid mx-auto w-[350px] xl:w-fit grid-cols-1 xl:grid-cols-4 mb-12">
-            {LearningGridArray.map((card, index) => {
-                return (
-                    <div className={`${index === 0 && "lg:col-span-2 lg:h-[294px] p-5"} ${ card.order % 2 === 1 ? "bg-[#2C333F] lg:h-[294px] p-5" : "bg-[#161D29] lg:h-[294px] p-5"} ${ card.order === 3 && "lg:col-start-2"} ${card.order < 0 && "bg-[#161D29]"}`} key={index}> 
-                            { card.order < 0 ? (
-                            <div className="xl:w-[90%] flex flex-col gap-3 pb-10 xl:pb-0">
-                                <div className="text-4xl font-semibold">
-                                    {card.heading}
-                                    <HighLightText text={card.highlightText}/>
-                                </div>
-                                <p className="text-[#838894] font-medium">
-                                    {card.description}
-                                </p>
-                                <div className="w-fit mt-2">
-                                    <CTAButton active={true} linkTo={card.BtnLink}>
-                                        {card.BtnText}
-                                    </CTAButton>
-                                </div>
+return(
+    <div className="grid mx-auto w-[350px] xl:w-fit grid-cols-1 xl:grid-cols-4 mb-12">
+        {LearningGridArray.map((card, index) => {
+            return (
+                <div className={`${index === 0 && "lg:col-span-2 lg:h-[294px] p-5"} ${ card.order % 2 === 1 ? "bg-[#2C333F] lg:h-[294px] p-5" : "bg-[#161D29] lg:h-[294px] p-5"} ${ card.order === 3 && "lg:col-start-2"} ${card.order < 0 && "bg-[#161D29]"}`} key={index}> 
+                        { card.order < 0 ? (
+                        <div className="xl:w-[90%] flex flex-col gap-3 pb-10 xl:pb-0">
+                            <div className="text-4xl font-semibold">
+                                {card.heading}
+                                <HighLightText text={card.highlightText}/>
                             </div>
-                        ) : (
-                            <div className="p-8 flex-col gap-8">
-                                <h1 className="text-[#F1F2FF] text-lg">{card.heading}</h1>
-                                <p className="text-[#838894] font-medium">
-                                    {card.description}
-                                </p>
+                            <p className="text-[#838894] font-medium">
+                                {card.description}
+                            </p>
+                            <div className="w-fit mt-2">
+                                <CTAButton active={true} linkTo={card.BtnLink}>
+                                    {card.BtnText}
+                                </CTAButton>
                             </div>
-                        )}
-                    </div>
-                )
-            })}
-        </div>
-    );
-};
+                        </div>
+                    ) : (
+                        <div className="p-8 flex-col gap-8">
+                            <h1 className="text-[#F1F2FF] text-lg">{card.heading}</h1>
+                            <p className="text-[#838894] font-medium">
+                                {card.description}
+                            </p>
+                        </div>
+                    )}
+                </div>
+            )
+        })}
+    </div>
+)};
 
 
 export {
