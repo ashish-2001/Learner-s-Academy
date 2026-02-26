1637ee9d4179d6867f3ea4f38e5647812a469e0b
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-26 16:02:47 +0530

    Added course builder form

--- src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
@@ -82,77 +82,77 @@ const CourseBuilderForm = () => {
   return (
     <div className="flex items-center justify-center">
       { loading ? (<div className="spinner"></div>) : (
-      <div className="space-y-8 rounded-md border-[1px] border-[#2C333F] bg-[#161D29] p-6">
-        <p className="text-2xl font-semibold text-[#F1F2FF]">Course Builder</p>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          <label className="text-sm text-[#F1F2FF]" htmlFor="sectionName">
-            Section Name<sup className="text-[#EF476F]">*</sup>
-          </label>
-          <input
-            id="sectionName"
-            placeholder="Add a section to build your course"
-            name="sectionName"
-            className="rounded-lg bg-[#2C333F] p-3 text-[16px] leading-[24px] text-[#F1F2FF] shadow-[0_1px_0_0] shadow-white/50 placeholder:text-[#6E727F] focus:outline-none w-full"
-            {...register("sectionName", { required: true })}
-          />
-          {errors.sectionName && (
-            <p className="ml-2 text-xs tracking-wide text-[#EF476F]">This field is required</p>
-          )}
-          <div className="flex items-end gap-x-4">
-            <button
-              type="submit"
-              className="flex items-center border border-[#FFD60A] bg-[#ffffff00] cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-[#000814] undefined"
-            >
-              <span className="text-[#FFD60A]">
-                {editSectionName ? "Edit Section Name" : "Create Section"}
-              </span>
-              <AiOutlinePlusCircle size={20} className="text-[#FFD60A]" />
-            </button>
-            {editSectionName && (
-              <button
-                onClick={() => {
-                  setEditSectionName(false);
-                  setValue("sectionName", "");
-                }}
-                type="button"
-                className="text-sm text-[#838894] underline"
-              >
-                Cancel Edit
-              </button>
+        <div className="space-y-8 rounded-md border-[1px] border-[#2C333F] bg-[#161D29] p-6">
+          <p className="text-2xl font-semibold text-[#F1F2FF]">Course Builder</p>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <label className="text-sm text-[#F1F2FF]" htmlFor="sectionName">
+              Section Name<sup className="text-[#EF476F]">*</sup>
+            </label>
+            <input
+              id="sectionName"
+              placeholder="Add a section to build your course"
+              name="sectionName"
+              className="rounded-lg bg-[#2C333F] p-3 text-[16px] leading-[24px] text-[#F1F2FF] shadow-[0_1px_0_0] shadow-white/50 placeholder:text-[#6E727F] focus:outline-none w-full"
+              {...register("sectionName", { required: true })}
+            />
+            {errors.sectionName && (
+              <p className="ml-2 text-xs tracking-wide text-[#EF476F]">This field is required</p>
             )}
-          </div>
-        </form>
-        {course.courseContent.length > 0 && <NestedView handleChangeEditSectionName={handleChangeEditSectionName} />}
-        <div className="flex justify-end gap-x-3">
-          <button
-            onClick={() => {
-              dispatch(setEditCourse(true));
-              dispatch(setStep(1));
-            }}
-            className="flex cursor-pointer items-center gap-x-2 rounded-md bg-[#838894] py-[8px] px-[20px] font-semibold text-[#000814]"
-          >
-            Back
-          </button>
-          <button
-            onClick={goNext}
-            className="flex items-center bg-[#FFD60A] cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-[#000814] undefined"
-          >
-            <span className="false">Next</span>
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 24 24"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
+            <div className="flex items-end gap-x-4">
+              <button
+                type="submit"
+                className="flex items-center border border-[#FFD60A] bg-[#ffffff00] cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-[#000814] undefined"
+              >
+                <span className="text-[#FFD60A]">
+                  {editSectionName ? "Edit Section Name" : "Create Section"}
+                </span>
+                <AiOutlinePlusCircle size={20} className="text-[#FFD60A]" />
+              </button>
+              {editSectionName && (
+                <button
+                  onClick={() => {
+                    setEditSectionName(false);
+                    setValue("sectionName", "");
+                  }}
+                  type="button"
+                  className="text-sm text-[#838894] underline"
+                >
+                  Cancel Edit
+                </button>
+              )}
+            </div>
+          </form>
+          {course.courseContent.length > 0 && <NestedView handleChangeEditSectionName={handleChangeEditSectionName} />}
+          <div className="flex justify-end gap-x-3">
+            <button
+              onClick={() => {
+                dispatch(setEditCourse(true));
+                dispatch(setStep(1));
+              }}
+              className="flex cursor-pointer items-center gap-x-2 rounded-md bg-[#838894] py-[8px] px-[20px] font-semibold text-[#000814]"
             >
-              <path fill="none" d="M0 0h24v24H0z"></path>
-              <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>
-            </svg>
-          </button>
+              Back
+            </button>
+            <button
+              onClick={goNext}
+              className="flex items-center bg-[#FFD60A] cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-[#000814] undefined"
+            >
+              <span className="false">Next</span>
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 24 24"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>
+              </svg>
+            </button>
+          </div>
         </div>
-      </div>
       )}
     </div>
   );
